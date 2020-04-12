dc8d2a96e61f8be81ef3e41905951beba8d74fbc
Ümit UZ <umituz998@gmail.com>
2020-04-12 18:53:06 +0300

    2017 practices added.

---
+++ 2017-practices/js/harici.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+	$("div.bir").click(function(){
+		$(this).toggleClass("iki");
+	});
+
+	$("div.ekle").click(function(){
+		$(this).addClass("uc");
+	});
+
+	$("div.dort").click(function(){
+		$(this).removeClass();
+	});
+
+});
